1fd4a92df8cc74222fa0d7d13fd85368ff3c4544
reskuy <reski.naksmd@gmail.com>
2021-08-26 13:37:22 +0800

    !

--- tss/service-worker.js
+++ tss/service-worker.js
@@ -1,3 +1,12 @@
 importScripts("precache-manifest.696172bac02de46a719e045a5d32ba83.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-self.addEventListener('fetch', function(event) {})
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.open('mysite-dynamic').then(function(cache) {
+        return fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+        });
+     })  
+    );
+  })
