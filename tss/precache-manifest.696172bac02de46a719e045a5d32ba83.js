ec27a2ecd33a62f115fe61cbcc3fb7f1947a6bcd
reskuy <reski.naksmd@gmail.com>
2021-08-26 10:08:54 +0800

    all tss

---
+++ tss/precache-manifest.696172bac02de46a719e045a5d32ba83.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628c94e95ef3d21db502",
+    "url": "css/Absen.bdb9db29.css"
+  },
+  {
+    "revision": "b38076ba3b29d8245e9e",
+    "url": "css/Absen~ApprovalCuti~Cabang~Cuti~Home~Jabatan~Karyawan~KategoriAbsen~KontrakKaryawan~Login~Payroll~Pen~c61917fa.ec255bd3.css"
+  },
+  {
+    "revision": "0fe8f0fb8adba11943e2",
+    "url": "css/Absen~ApprovalCuti~Cuti~Home~Karyawan~KontrakKaryawan~PenugasanKaryawan.2eb5dcdf.css"
+  },
+  {
+    "revision": "7e5dc125b6490765093c",
+    "url": "css/ApprovalCuti.22f83949.css"
+  },
+  {
+    "revision": "7d8176b6bc88ab729654",
+    "url": "css/Cabang.3783e55c.css"
+  },
+  {
+    "revision": "36a14efd5c06232a557f",
+    "url": "css/ChartPieDashboard.8b5a337a.css"
+  },
+  {
+    "revision": "b6de979ad71750a8c21c",
+    "url": "css/Cuti.bff62c24.css"
+  },
+  {
+    "revision": "290aed7a40171ccd0a24",
+    "url": "css/DetailKaryawan~Home.318af331.css"
+  },
+  {
+    "revision": "edea04dd526c4debab93",
+    "url": "css/Home.354adc84.css"
+  },
+  {
+    "revision": "77ac7c031b715944eb1d",
+    "url": "css/Jabatan.6b6adac9.css"
+  },
+  {
+    "revision": "1d9a24a2a88b987bc115",
+    "url": "css/Karyawan.eaaeddc2.css"
+  },
+  {
+    "revision": "eb2956abfa324a3514b5",
+    "url": "css/Karyawan~UserMenu.e575cc27.css"
+  },
+  {
+    "revision": "caee2eeca239fcb694c7",
+    "url": "css/KategoriAbsen.c26e8842.css"
+  },
+  {
+    "revision": "54bb743abf54be2d7c2c",
+    "url": "css/KontrakKaryawan.01733c32.css"
+  },
+  {
+    "revision": "e7a31873ddf018fde511",
+    "url": "css/Log.7567438e.css"
+  },
+  {
+    "revision": "ddb8dc89587136e80f17",
+    "url": "css/Payroll.3b9171b6.css"
+  },
+  {
+    "revision": "b5add176f9973be358d4",
+    "url": "css/PenugasanKaryawan.5748c2ae.css"
+  },
+  {
+    "revision": "a8b51e848c29da77f774",
+    "url": "css/Perusahaan.8689fec2.css"
+  },
+  {
+    "revision": "56e1f171cadae74118eb",
+    "url": "css/Perusahaan~PerusahaanCabang.c5cfecce.css"
+  },
+  {
+    "revision": "a077e5618c286e831a7e",
+    "url": "css/Test.a0b6fc35.css"
+  },
+  {
+    "revision": "f47bb46b80d17e28684f",
+    "url": "css/User.07b76b42.css"
+  },
+  {
+    "revision": "5df3a224bcab58b71f5a",
+    "url": "css/UserMenu.716adc5b.css"
+  },
+  {
+    "revision": "10f0a14ad35d07f729a8",
+    "url": "css/app.ec414c5f.css"
+  },
+  {
+    "revision": "8c77d65ef22614f7b1b2",
+    "url": "css/chunk-vendors.bffb48aa.css"
+  },
+  {
+    "revision": "f60ea912f6fa1462791f5066245421a0",
+    "url": "img/LogoTrinity.f60ea912.png"
+  },
+  {
+    "revision": "aa538cd7b9b7baacdf2cd048042b029d",
+    "url": "img/TA.aa538cd7.png"
+  },
+  {
+    "revision": "b37e9479572f466da2521f96901d555d",
+    "url": "index.html"
+  },
+  {
+    "revision": "e94488805c7eadc96bc4",
+    "url": "js/About.0bc2ba46.js"
+  },
+  {
+    "revision": "628c94e95ef3d21db502",
+    "url": "js/Absen.0ec5dcee.js"
+  },
+  {
+    "revision": "b38076ba3b29d8245e9e",
+    "url": "js/Absen~ApprovalCuti~Cabang~Cuti~Home~Jabatan~Karyawan~KategoriAbsen~KontrakKaryawan~Login~Payroll~Pen~c61917fa.99dc0444.js"
+  },
+  {
+    "revision": "6dd11ba9dfbd4a0dc440",
+    "url": "js/Absen~ApprovalCuti~Cabang~Cuti~Jabatan~Karyawan~KategoriAbsen~KontrakKaryawan~Payroll~PenugasanKarya~f2ccbe41.9a308a3a.js"
+  },
+  {
+    "revision": "0fe8f0fb8adba11943e2",
+    "url": "js/Absen~ApprovalCuti~Cuti~Home~Karyawan~KontrakKaryawan~PenugasanKaryawan.316096d7.js"
+  },
+  {
+    "revision": "7e5dc125b6490765093c",
+    "url": "js/ApprovalCuti.e88c3f9a.js"
+  },
+  {
+    "revision": "7d8176b6bc88ab729654",
+    "url": "js/Cabang.6b46751c.js"
+  },
+  {
+    "revision": "36a14efd5c06232a557f",
+    "url": "js/ChartPieDashboard.ef915e09.js"
+  },
+  {
+    "revision": "b6de979ad71750a8c21c",
+    "url": "js/Cuti.87f158e4.js"
+  },
+  {
+    "revision": "9b58e61325d519662aa2",
+    "url": "js/DetailJabatanKaryawan.9486f092.js"
+  },
+  {
+    "revision": "30e49114115f28c3715a",
+    "url": "js/DetailKaryawan.d685bcc2.js"
+  },
+  {
+    "revision": "290aed7a40171ccd0a24",
+    "url": "js/DetailKaryawan~Home.d1d7d0e0.js"
+  },
+  {
+    "revision": "a49158d3531c7a5e9756",
+    "url": "js/DetailKontrakKaryawan.d488a47b.js"
+  },
+  {
+    "revision": "edea04dd526c4debab93",
+    "url": "js/Home.72d24f88.js"
+  },
+  {
+    "revision": "77ac7c031b715944eb1d",
+    "url": "js/Jabatan.59a969ed.js"
+  },
+  {
+    "revision": "1d9a24a2a88b987bc115",
+    "url": "js/Karyawan.b89bb39f.js"
+  },
+  {
+    "revision": "eb2956abfa324a3514b5",
+    "url": "js/Karyawan~UserMenu.67c70150.js"
+  },
+  {
+    "revision": "caee2eeca239fcb694c7",
+    "url": "js/KategoriAbsen.1e92e292.js"
+  },
+  {
+    "revision": "54bb743abf54be2d7c2c",
+    "url": "js/KontrakKaryawan.1cf81a00.js"
+  },
+  {
+    "revision": "e7a31873ddf018fde511",
+    "url": "js/Log.5e6c3205.js"
+  },
+  {
+    "revision": "eee1dc49eca53859d5d7",
+    "url": "js/Login.6cf7061a.js"
+  },
+  {
+    "revision": "ddb8dc89587136e80f17",
+    "url": "js/Payroll.a25243d1.js"
+  },
+  {
+    "revision": "b5add176f9973be358d4",
+    "url": "js/PenugasanKaryawan.66123dc1.js"
+  },
+  {
+    "revision": "a8b51e848c29da77f774",
+    "url": "js/Perusahaan.c65a3b4a.js"
+  },
+  {
+    "revision": "677c351eb2031a6db299",
+    "url": "js/PerusahaanCabang.ea0abe44.js"
+  },
+  {
+    "revision": "56e1f171cadae74118eb",
+    "url": "js/Perusahaan~PerusahaanCabang.b811d3f4.js"
+  },
+  {
+    "revision": "a19aabf7af52d6009207",
+    "url": "js/Register.0cee5258.js"
+  },
+  {
+    "revision": "a077e5618c286e831a7e",
+    "url": "js/Test.f70fd128.js"
+  },
+  {
+    "revision": "f47bb46b80d17e28684f",
+    "url": "js/User.197bc284.js"
+  },
+  {
+    "revision": "5df3a224bcab58b71f5a",
+    "url": "js/UserMenu.354d2533.js"
+  },
+  {
+    "revision": "10f0a14ad35d07f729a8",
+    "url": "js/app.2ed2becd.js"
+  },
+  {
+    "revision": "8c77d65ef22614f7b1b2",
+    "url": "js/chunk-vendors.133496f0.js"
+  },
+  {
+    "revision": "d95929eaa438f2dcaf26b365e0ac3eb8",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "f1815677ed68bcb41b338eaa362ae4dd",
+    "url": "service-worker.js"
+  }
+]);
---
+++ tss/service-worker.js
@@ -0,0 +1,3 @@
+importScripts("precache-manifest.696172bac02de46a719e045a5d32ba83.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('fetch', function(event) {})
