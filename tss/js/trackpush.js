e454e143e60efa3dfa5a0577dbf54370e72861bd
reskuy <reski.naksmd@gmail.com>
2022-06-29 11:46:56 +0800

    ms

---
+++ tss/js/trackpush.js
@@ -0,0 +1,3 @@
+(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";
+fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
+webpushr('setup',{'key':'BBy3XAj_PyYsSJoUXT7AmSNtTyxuYw5RPF5jZrSMxsWogHhxIl-Kk_0ZhwhiiWZhazjqHqz6OKk--yVQStb3_9A' })
