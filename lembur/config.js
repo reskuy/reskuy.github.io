0515543fea636e2571697569aed57efd04971f5b
unknown <reski.w2001@gmail.com>
2025-05-16 21:45:18 +0800

    U

---
+++ lembur/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlccyouywuj9g8euSsPdZBWOet_nmg06E",
+    authDomain: "app-lembur-amartha.firebaseapp.com",
+    projectId: "app-lembur-amartha",
+    storageBucket: "app-lembur-amartha.firebasestorage.app",
+    messagingSenderId: "68667078727",
+    appId: "1:68667078727:web:08582e9b671529c2ca5962",
+    measurementId: "G-TT8ZWMLSSP"
+};
+export {firebaseConfig};
